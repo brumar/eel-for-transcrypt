9cc410533ae702da162af06ae34df61c85dd07a1
Bruno <martin.bruno.mail@gmail.com>
2018-12-05 15:29:08 +0100

    update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup_requirements = ['pytest-runner', ]
 test_requirements = ['pytest', ]
 
 setup(
-    author="Bruno Martin",
+    author="Chris Knott, Bruno Martin",
     author_email='martin.bruno.mail@gmail.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -41,6 +41,13 @@ setup(
     keywords='eel_for_transcrypt',
     name='eel_for_transcrypt',
     packages=find_packages(include=['eel_for_transcrypt']),
+    package_data={
+        'eel_for_transcrypt': ['eel.js'],
+    },
+    install_requires=['bottle', 'bottle-websocket', 'future', 'whichcraft'],
+    description='eel fork optimized for transcrypt',
+    long_description=open('README.md', encoding='utf-8').readlines()[1],
+    keywords=['gui', 'transcrypt', 'html', 'javascript', 'electron'],
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
